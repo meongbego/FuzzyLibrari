8ecc00443feaf651549ccf762ab14544c6184338
meongbego <meongbego@gmail.com>
2014-11-17 18:42:23 +0800

    Commits Two : Penambahan fungsi S Naik

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <QCoreApplication>
+#include <QDebug>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
--- snaik.h
+++ snaik.h
@@ -2,12 +2,35 @@
 #define SNAIK_H
 
 #include "fuzzyset.h"
+#include <math.h>
 
 class sNaik : public FuzzySet
 {
     Q_OBJECT
 public:
     explicit sNaik(QObject *parent = 0);
+
+private:
+    double tengahBeta;
+public:
+    void setTengahSnaik(double b){
+        tengahBeta=b;
+    }
+
+    double getValue(double v){
+        if (v<=nKiri){
+            return 0;
+        }
+        else if ((nKiri<v)&& (v<tengahBeta)){
+            return (2*pow(((v-nKiri)/(nKanan-nKiri)),2));
+        }
+        else if ((tengahBeta<v)&&(v<nKanan)){
+            return (1-2*pow(((nKanan-v)/(nKanan-nKiri)),2));
+        }
+        else if(v>=nKanan){
+            return 1;
+        }
+    }
 
 signals:
 
