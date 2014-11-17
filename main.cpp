6e2e20a3a476490494f7045cc016fbb570c4563e
meongbego <meongbego@gmail.com>
2014-11-17 23:15:53 +0800

    add example

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 #include <segitiga.h>
 #include <trapesium.h>
 #include <fuzzyset.h>
+#include <iostream>
+using namespace std;
 
 const double cdMinimumPrice =0;
-const double cdMaximumPrice =70;
+const double cdMaximumPrice =100;
 
 int main(int argc, char *argv[])
 {
@@ -15,27 +17,36 @@ int main(int argc, char *argv[])
 
     FuzzySet *fuzzy[2];
     fuzzy[0] = new Segitiga;
+    fuzzy[1] = new Segitiga;
     //fuzzy[1] = new Trapesium;
 
     fuzzy[0]->setInterval(0,50);
     fuzzy[0]->setTengah(25,0);
     fuzzy[0]->setNama("pendek");
-    fuzzy[0]->setNamaVariabel('p');
+    fuzzy[0]->setType('p');
 
-    //if(10<cdMinimumPrice) continue;
-    //if(10>cdMaximumPrice) continue;
+    fuzzy[1]->setInterval(40,70);
+    fuzzy[1]->setTengah(60,0);
+    fuzzy[1]->setNama("tinggi");
+    fuzzy[1]->setType('n');
+    double dValue;
+      cout<<"Masukan Nilai : "; cin>>dValue;
+      for(int i=0; i<2; i++)
+      {
+         cout<<"\n Nilai = "<<dValue<<endl;
+         if(fuzzy[i]->isDotInInterval(dValue)){
+             cout<<"Nilai Masuk";
+         }
+         else{
+             cout<<"Nilai Tidak Masuk";
+         }
+         cout<<endl;
+         cout<<"Fungsi Yang Digunakan = "<<endl;
+         fuzzy[i]->getName();
+         cout<<"Masuk Di Himpunan = ";
+         cout<<fuzzy[i]->getValue(dValue)<<endl;
 
-    for(int i=0; i<3; i++)
-    {
-       if (fuzzy[i]->isDotInInterval(10))
-           qDebug()<<"Yes";
-       else
-           qDebug()<<"No";
-
-       fuzzy[i]->getName();
-
-       qDebug()<<fuzzy[i]->getValue(10);
-    }
+      }
 
     return a.exec();
 }
